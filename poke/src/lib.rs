c7c9a3800867866bc4936e92f1f68a6d3a0c64d5
Tobias Merkle <tobias@branchmessenger.com>
2024-07-17 11:29:05 -0400

    🫵

---
+++ poke/src/lib.rs
@@ -0,0 +1,89 @@
+use crate::kinode::process::poke::{Request as PokeRequest, Response as PokeResponse, SendRequest};
+use crate::kinode::process::notify::{Request as NotifyRequest, Response as NotifyResponse};
+use kinode_process_lib::homepage::add_to_homepage;
+use kinode_process_lib::{await_message, call_init, println, Address, Message, Request, Response};
+
+wit_bindgen::generate!({
+    path: "target/wit",
+    world: "poke-template-dot-os-v0",
+    generate_unused_types: true,
+    additional_derives: [serde::Deserialize, serde::Serialize, process_macros::SerdeJsonInto],
+});
+
+fn handle_message(
+    our: &Address,
+    message: &Message,
+) -> anyhow::Result<()> {
+    if !message.is_request() {
+        return Err(anyhow::anyhow!("unexpected Response: {:?}", message));
+    }
+
+    let body = message.body();
+    let source = message.source();
+    match body.try_into()? {
+        PokeRequest::Send(SendRequest {
+            ref target,
+        }) => {
+            if target == &our.node {
+                if source.node == *target {
+                    println!("you poked yourself");
+                } else {
+                    println!("{} poked you", source.node);
+                }
+                let Ok(_) = Request::new()
+                    .target(Address {
+                        node: our.node,
+                        process: "notify:notify:tantum-ergo.os".parse()?,
+                    })
+                    .body(NotifyRequest::Push {
+                        title: "You got poked!",
+                        body: format!("{} poked you", source.node),
+                    })
+                    .send_and_await_response(5) 
+                else {
+                    return Err(anyhow::anyhow!("{} is offline or doesn't have notify", target));
+                };
+            } else {
+                let Ok(Ok(Message::Response { body, .. })) = Request::new()
+                    .target(Address {
+                        node: target.clone(),
+                        process: "poke:poke:tantum-ergo.os".parse()?,
+                    }) 
+                    .body(body)
+                    .send_and_await_response(5)
+                else {
+                    return Err(anyhow::anyhow!("{} is offline or doesn't have poke", target));
+                };
+            }
+            Response::new()
+                .body(PokeResponse::Send)
+                .send()
+                .unwrap();
+        }
+    }
+    Ok(())
+}
+
+const ICON: &str = include_str!("ICON");
+
+call_init!(init);
+fn init(our: Address) {
+    println!("begin");
+
+    add_to_homepage(
+        "Poke",
+        Some(ICON),
+        None,
+        None,
+    );
+
+    loop {
+        match await_message() {
+            Err(send_error) => println!("got SendError: {send_error}"),
+            Ok(ref message) => match handle_message(&our, message) {
+                Ok(_) => {}
+                Err(e) => println!("got error while handling message: {e:?}"),
+            }
+        }
+    }
+}
---
+++ send/src/lib.rs
@@ -0,0 +1,47 @@
+use std::str::FromStr;
+
+use crate::kinode::process::poke::{Request as PokeRequest, Response as ChatResponse, SendRequest};
+use kinode_process_lib::{
+    await_next_message_body, call_init, println, Address, Message, Request,
+};
+
+wit_bindgen::generate!({
+    path: "target/wit",
+    world: "poke-template-dot-os-v0",
+    generate_unused_types: true,
+    additional_derives: [serde::Deserialize, serde::Serialize],
+});
+
+call_init!(init);
+fn init(our: Address) {
+    let Ok(body) = await_next_message_body() else {
+        println!("failed to get args!");
+        return;
+    };
+
+    let args = String::from_utf8(body).unwrap_or_default();
+
+    let Ok(target) = Address::from_str(&args) else {
+        println!("usage:\nsend:poke:template.os target");
+        return;
+    };
+
+    let Ok(Ok(Message::Response { body, .. })) =
+        Request::to((our.node(), ("poke", "poke", "template.os")))
+            .body(
+                serde_json::to_vec(&PokeRequest::Send(SendRequest {
+                    target: target.to_string(),
+                }))
+                .unwrap(),
+            )
+            .send_and_await_response(5)
+    else {
+        println!("did not receive expected Response from poke:poke:template.os");
+        return;
+    };
+
+    let Ok(ChatResponse::Send) = serde_json::from_slice(&body) else {
+        println!("did not receive expected Ack from poke:poke:template.os");
+        return;
+    };
+}
